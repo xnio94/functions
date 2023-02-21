51df39e57038cbca22b0448e40a3a3997ca3748a
xnio94 <xnio94@gmail.com>
2023-02-21 18:38:36 +0100

    last_commit

--- is_consecutive.py
+++ is_consecutive.py
@@ -3,12 +3,14 @@ import random
 import string
 import subprocess
 
-from functions.merge_videos import merge_videos
-from functions.remove_file import remove_file
-from functions.scene_detect import scene_detect
+from functions.dynamic_import import dynamic_import
 
 
 def is_consecutive(video1, video2):
+    merge_videos = dynamic_import('merge_videos')
+    remove_file = dynamic_import('remove_file')
+    scene_detect = dynamic_import('scene_detect')
+
     print("#########>>is_consecutive start : ")
     video1_end = video1[:-4] + ''.join(
         random.choices(string.ascii_letters + string.digits, k=16)) + '.mp4'
@@ -18,13 +20,10 @@ def is_consecutive(video1, video2):
         random.choices(string.ascii_letters + string.digits, k=16)) + '.mp4'
     duration = "0.2"
 
-
-
     print("#########>> .mp4 : ")
     print(glob.glob('*.mp4'))
     print("#########>> all files : ")
     print(glob.glob('*'))
-
 
     print("#########>>" + video1_end + " start : ")
     # command = f'ffmpeg -sseof -{duration} -i "{video1}" -c copy "{video1_end}"'
@@ -37,20 +36,16 @@ def is_consecutive(video1, video2):
     print("#########>> all files : ")
     print(glob.glob('*'))
 
-
-
     print("#########>>" + video2_start + " start : ")
     # command = f'ffmpeg -t {duration} -i "{video2}" -c copy "{video2_start}"'
     command = ["ffmpeg", "-t", f"{duration}", "-i", video2, "-c", "copy", video2_start]
     print("#########>>" + str(command))
     subprocess.run(command)
 
-
     print("#########>> .mp4 : ")
     print(glob.glob('*.mp4'))
     print("#########>> all files : ")
     print(glob.glob('*'))
-
 
     print("#########>>merge_videos start : ")
     merge_videos([video1_end, video2_start], merged_result)
