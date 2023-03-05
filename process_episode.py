5d4f79dd682d37d525a7481aad83d303b630c507
xnio94 <xnio94@gmail.com>
2023-03-05 07:09:51 +0100

    last_commit

--- process_episode.py
+++ process_episode.py
@@ -63,15 +63,14 @@ def process_episode(request):
         anas_link = "https://openailabsprodscus.blob.core.windows.net/private/user-P0iWohdGZ4CQCPD526WEhTBT/generations/generation-9vfxXLijh0NN3RGVVpoQBe0b/image.webp?st=2023-03-05T04%3A38%3A53Z&se=2023-03-05T06%3A36%3A53Z&sp=r&sv=2021-08-06&sr=b&rscd=inline&rsct=image/webp&skoid=15f0b47b-a152-4599-9e98-9cb4a58269f8&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2023-03-05T03%3A10%3A54Z&ske=2023-03-12T03%3A10%3A54Z&sks=b&skv=2021-08-06&sig=idUX5oT7SjOblKb1iRck8GmCA4sJNC48hWh%2BuRkNoys%3D"
 
 
-        # url = request.args.get('url', '')
-        response = requests.get(anas_link)
-        return response.content, response.status_code, response.headers.items()
+        # # url = request.args.get('url', '')
+        # response = requests.get(anas_link)
+        # return response.content, response.status_code, response.headers.items()
 
 
-
-        # f = urllib.request.urlopen(anas_link)
-        # with open("filename", "wb") as imgFile:
-        #     imgFile.write(f.read())
+        f = urllib.request.urlopen(anas_link)
+        with open("filename", "wb") as imgFile:
+            imgFile.write(f.read())
 
         # urllib.request.urlretrieve(anas_link, 'filename',)
         return (send_file('filename',
